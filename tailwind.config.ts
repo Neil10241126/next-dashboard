efc279e745397e814da1156b7ee4c493a8364788
Neil <Neil10241126@gmail.com>
2025-01-25 14:19:31 +0800

    Setup Fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -39,6 +39,10 @@ export default {
       screens: {
         "xs": "420px"
       },
+      fontFamily: {
+        "inter": ['var(--font-inter)'],
+        "space-grotesk": ["var(--font-space-grotesk)"]
+      }
     },
   },
   plugins: [],
